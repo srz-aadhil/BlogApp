3cf9e07bf8930566a9d383c421cd55c110d20a38
srz-aadhil <aadhil.ahammad@smartrabbitz.com>
2024-10-04 18:07:08 +0530

    UpdateAuthor Function Test

--- app/controller/author_controller_test.go
+++ app/controller/author_controller_test.go
@@ -74,4 +74,137 @@ func TestGetOneAuthor(t *testing.T) {
 
 }
 
-func TestGetAllAuthors(t *testing.T)
+func TestGetAllAuthors(t *testing.T) {
+	createdAt := time.Date(2024, time.July, 15, 0, 0, 0, 0, time.UTC)
+	updatedAt := time.Date(2024, time.July, 15, 0, 0, 0, 0, time.UTC)
+	authorMock := new(mocks.AuthorService)
+	conn := NewAuthorController(authorMock)
+	tests := []struct {
+		name    string
+		status  int
+		author  *[]dto.AuthorResponse
+		want    string //dto.AuthorResponse
+		error   error
+		wantErr bool
+	}{
+		//Success case
+		{
+			name:   "success case",
+			status: 200,
+			author: &[]dto.AuthorResponse{
+				{
+					ID:   2,
+					Name: "testing 1",
+					CreatedUpdatedResponse: dto.CreatedUpdatedResponse{
+						CreatedAt: createdAt,
+						UpdatedAt: &updatedAt,
+					},
+					DeleteResponse: dto.DeleteResponse{
+						DeletedAt: &createdAt,
+						DeletedBy: nil,
+					},
+				},
+				{
+					ID:   3,
+					Name: "testing 2",
+					CreatedUpdatedResponse: dto.CreatedUpdatedResponse{
+						CreatedAt: createdAt,
+						UpdatedAt: &updatedAt,
+					},
+					DeleteResponse: dto.DeleteResponse{
+						DeletedBy: nil,
+						DeletedAt: nil,
+					},
+				},
+			},
+			want:    `{"status":"ok","result":[{"id":2,"name":"testing 1","created_at":"2024-07-15T00:00:00Z","updated_at":"2024-07-15T00:00:00Z","deleted_at":"2024-07-15T00:00:00Z"},{"id":3,"name":"testing 2","created_at":"2024-07-15T00:00:00Z","updated_at":"2024-07-15T00:00:00Z"}]}`,
+			wantErr: false,
+		},
+
+		{
+			name:   "error case",
+			status: 500,
+			author: nil,
+			error: &e.WrapError{
+				ErrorCode: 500,
+				Msg:       "Internal Server Error",
+				RootCause: errors.New("database error"),
+			},
+			want:    `{"status":"not ok","error":{"code":500,"message":"can't get all authors","details":["database error"]}}`,
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/", nil)
+			res := httptest.NewRecorder()
+			authorMock.On("GetAuthors").Once().Return(test.author, test.error)
+			conn.GetaAllAuthors(res, req)
+
+			assert.Equal(t, test.status, res.Code)
+			assert.Equal(t, test.want, res.Body.String())
+
+		})
+	}
+
+}
+
+func TestUpdateAuthor(t *testing.T) {
+	createdAt := time.Date(2024, time.July, 15, 0, 0, 0, 0, time.UTC)
+	updatedAt := time.Date(2024, time.July, 15, 0, 0, 0, 0, time.UTC)
+	authorMock := new(mocks.AuthorService)
+	conn := NewAuthorController(authorMock)
+	tests := []struct {
+		name    string
+		status  int
+		author  *dto.AuthorResponse
+		want    string //dto.AuthoResponse
+		error   error
+		wantErr bool
+	}{
+		//Success case
+		{
+			name:   "success case",
+			status: 200,
+			author: &dto.AuthorResponse{
+				ID:   3,
+				Name: "updated author",
+				CreatedUpdatedResponse: dto.CreatedUpdatedResponse{
+					CreatedAt: createdAt,
+					UpdatedAt: &updatedAt,
+				},
+				DeleteResponse: dto.DeleteResponse{
+					DeletedBy: nil,
+					DeletedAt: nil,
+				},
+			},
+			want:    `{"status":"ok","result":"Author Updation Success"}`,
+			wantErr: false,
+		},
+		{
+			//error case
+			name:   "error case",
+			status: 500,
+			error: &e.WrapError{
+				ErrorCode: 500,
+				Msg:       "Internal Server Error",
+				RootCause: errors.New("database error"),
+			},
+			want:    `{"status":"not ok","error":{"code":500,"message":"can't update author","details":["database error"]}}`,
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req := httptest.NewRequest("POST", "/2", nil)
+			res := httptest.NewRecorder()
+			authorMock.On("UpdateAuthor", req).Once().Return(test.error)
+			conn.UpdateAuthor(res, req)
+
+			assert.Equal(t, test.status, res.Code)
+			assert.Equal(t, test.want, res.Body.String())
+		})
+	}
+}
